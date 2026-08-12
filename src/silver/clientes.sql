acc3fb14046bda2bfa32da3f562133067057cb37
joao.carvalho.suhett@gmail.com <joao.carvalho.suhett@gmail.com>
2026-08-12 23:08:31 +0000

    feat: Ingestão em silver
    - clientes
    - produtos
    - transações
    - transação produto

---
+++ src/silver/clientes.sql
@@ -0,0 +1,11 @@
+SELECT 
+    idCliente,
+    flEmail,
+    flTwitch,
+    flYouTube,
+    flBlueSky,
+    flInstagram,
+    qtdePontos,
+    DtCriacao,
+    DtAtualizacao
+FROM bronze.upsell.clientes
---
+++ src/silver/transacao_produto.sql
@@ -0,0 +1,7 @@
+SELECT 
+    idTransacaoProduto,
+    IdTransacao,
+    IdProduto,
+    QtdeProduto,
+    vlProduto
+FROM bronze.upsell.transacao_produto
